a2df8d9ba99c912a8af71993c638228fca5be5bb
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-29 23:44:41 +0900

    add: 3の倍数のテスト

--- testFizzBuzz.php
+++ testFizzBuzz.php
@@ -14,9 +14,15 @@ class FizzBuzzTest extends TestCase
     //     $this->assertEquals(0, count($stack));
     // }
 
-    public function test__数字が入力されたら文字を返す(){
+    public function test__数字が入力されたら文字を返す() {
         $fizzBuzz = new FizzBuzz();
         $data = $fizzBuzz->returnString();
-        $this->assertEquals($data, "hoge");
+        $this->assertEquals($data, 'hoge');
+    }
+
+    public function test__3の倍数を入力したらFizzと返して出力する() {
+        $fizzBuzz = new FizzBuzz();
+        $data = $fizzBuzz->fizzBuzz(3);
+        $this->assertEquals($data, 'Fizz');
     }
 }
