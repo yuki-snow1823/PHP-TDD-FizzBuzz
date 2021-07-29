8bdf5027b0f96394e53f27079d2c8c58c07bd8b3
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-29 14:02:53 +0900

    add: testのtest

--- testFizzBuzz.php
+++ testFizzBuzz.php
@@ -1,8 +1,15 @@
 <?php
+include './FizzBuzz.php';
 
 use PHPUnit\Framework\TestCase;
 
-class HogeTest extends TestCase
+class FizzBuzzTest extends TestCase
 {
-    
+    // public function test__テストが正常に動くかのテスト()
+    // {
+    //     $stack = [];
+    //     $this->assertEquals(0, count($stack));
+    // }
+
+
 }
